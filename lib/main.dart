78faeda1676e5b17afc452aa030c09de8f83aa5b
Pablo <pablo_verdugoj@hotmail.com>
2026-09-18 19:10:11 -0300

    Definiendo Tiles y Estilos UI ademas de color segun caso

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'game.dart';
+
 void main() {
   //Corre un widget desde la root raiz del proyecto
   runApp(const MainApp());
@@ -11,7 +13,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Soy la bomba!'))),
+      home: Scaffold(body: Center(child: Tile('A', HitType.hit))),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+        //TODO: add children
+      ),
     );
   }
 }
